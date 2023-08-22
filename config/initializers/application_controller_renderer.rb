949e534f796ce85f7f82943e2c67a0ad9a087e3f
EC2 Default User <ec2-user@ip-172-31-11-49.ap-northeast-1.compute.internal>
2023-08-22 03:35:35 +0000

    unadded file is all added

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'e93c0d3f543f0722eee0524d3c37f200a384cb1e5c24e5ad7f65b58df3211951d7eae04db9855fee234f0fb5bc9f0cb394a93eb7e0d58718ed13216c49edff8c'
